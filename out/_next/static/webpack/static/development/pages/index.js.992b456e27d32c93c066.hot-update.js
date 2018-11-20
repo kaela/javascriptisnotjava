c61ee1c925b8e95ca4b4369781570e6c3f184e08
Kaela <kaela@mediahound.com>
2018-11-20 14:06:44 -0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/webpack/static/development/pages/index.js.992b456e27d32c93c066.hot-update.js
@@ -0,0 +1,159 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/footer.js":
+/*!******************************!*\
+  !*** ./components/footer.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../theme/theme.js */ "./theme/theme.js");
+var _jsxFileName = "/Users/kaela/Sites/javascriptisnotjava/components/footer.js";
+
+function _templateObject4() {
+  var data = _taggedTemplateLiteral(["\n  margin-top: ", "px;\n  & img {\n    background-color: ", ";\n    margin: ", "px;\n  }\n"]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = _taggedTemplateLiteral(["\n  font-size: 16px;\n  font-style: italic;\n  margin: 0 auto ", "px;\n"]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = _taggedTemplateLiteral(["\n  ", ";\n"]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  background: ", ";\n  color: ", ";\n  height: ", "px;\n  ", ";\n  line-height: ", "px;\n"]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+
+
+
+
+var Container = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].header(_templateObject(), _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["PrimaryText"], _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["Background"], _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["NavHeight"], ""
+/* Accommodate bottom border */
+, _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["NavHeight"] - 3);
+var Content = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject2(), _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["Container"]());
+var P = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].p(_templateObject3(), _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["Padding"] * 2);
+var SocialIcons = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].div(_templateObject4(), _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["Padding"], _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["PrimaryColor"], _theme_theme_js__WEBPACK_IMPORTED_MODULE_3__["Padding"] / 2);
+
+var SocialLink = function SocialLink(_ref) {
+  var href = _ref.href,
+      title = _ref.title,
+      children = _ref.children;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_2___default.a, {
+    href: href,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 33
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    title: title,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 34
+    },
+    __self: this
+  }, children));
+};
+
+var Footer = function Footer() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Container, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 39
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Content, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 40
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(SocialIcons, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 41
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(SocialLink, {
+    href: "http://linkedin.com/in/kaelanichols",
+    title: "linkedin",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 42
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "static/assets/linkedin.svg",
+    alt: "LinkedIn",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 43
+    },
+    __self: this
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(SocialLink, {
+    href: "http://github.com/kaela",
+    title: "github",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 45
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "static/assets/github.svg",
+    alt: "github",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 46
+    },
+    __self: this
+  }))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(P, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 49
+    },
+    __self: this
+  }, "javascriptisnotjava.io reserves the right to remove any content for any reason at any time. Because it's my site, and I can.")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Footer);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.992b456e27d32c93c066.hot-update.js.map
