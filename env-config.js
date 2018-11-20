e5b0f399180b7df5a9f3706484e0e16eb665c99d
Kaela <kaela@mediahound.com>
2018-11-20 15:19:19 -0800

    finally fixed this thing?

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  "process.env.BACKEND_URL": prod ? "/javascriptisnotjava" : ""
+  "process.env.BACKEND_URL": prod ? "/" : ""
 };
--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     };
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? "/javascriptisnotjava/" : "",
+  assetPrefix: !debug ? "/" : "",
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
